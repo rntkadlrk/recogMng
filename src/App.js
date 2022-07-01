54086e014d1d763981056f6aea3114964d365b03
jhKim <rntkadlrk@gmail.com>
2022-07-01 17:55:28 +0900

    MUI Custom 작업중

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Components } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Table from '@mui/material/Table';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import TableHead from '@mui/material/TableHead';
+import { withStyles } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const styles = theme => ({
+  root:{
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table:{
+    minWidth: 1080
+  }
+})
 
 const customers = [{
   'id':1,
@@ -28,26 +46,29 @@ const customers = [{
 }]
 
 
-function App() {
-  return (
-    <div>
-    {
-      customers.map(c => {
-        return(
-          <Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-          />
-        )
-      })
-    }
-    </div>
+function App(props) {
+  const {classes} = props.styles;
+  return(
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년원일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead> 
+        <TableBody>
+         
+          {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+          
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
